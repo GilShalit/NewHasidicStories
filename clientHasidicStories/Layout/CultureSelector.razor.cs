371e9ab926f09c4da22d922d9debc451a14e510f
Gil Shalit <gil.shalit@gmail.com>
2024-04-05 15:31:39 +0300

    Localization (not working)

---
+++ clientHasidicStories/Layout/CultureSelector.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace clientHasidicStories.Layout
+{
+	public partial class CultureSelector
+	{
+		[Inject]
+		public NavigationManager NavManager { get; set; }
+
+		[Inject]
+		public IJSRuntime JSRuntime { get; set; }
+
+		CultureInfo[] cultures = new[]
+		{
+			new CultureInfo("en-US"),
+			new CultureInfo("de-DE"),
+			new CultureInfo("he-IL")
+		};
+
+		CultureInfo Culture
+		{
+			get => CultureInfo.CurrentCulture;
+			set
+			{
+				if (CultureInfo.CurrentCulture != value)
+				{
+					var js = (IJSInProcessRuntime)JSRuntime;
+					js.InvokeVoid("blazorCulture.set", value.Name);
+
+					NavManager.NavigateTo(NavManager.Uri, forceLoad: true);
+				}
+			}
+		}
+	}
+}
--- clientHasidicStories/Program.cs
+++ clientHasidicStories/Program.cs
@@ -1,11 +1,19 @@
 using clientHasidicStories;
+using clientHasidicStories.Extensions;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Localization;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var app=builder.Build();
+
+await app.SetDefaultCulture();
+
+await app.RunAsync();
