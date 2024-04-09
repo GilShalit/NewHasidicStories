2f0a43f5d9d5780ee3e1a92a409fecb8500cf70a
Gil Shalit <gil.shalit@gmail.com>
2024-04-09 22:05:54 +0300

    Blazorise

--- clientHasidicStories/Program.cs
+++ clientHasidicStories/Program.cs
@@ -3,14 +3,25 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Globalization;
 using Microsoft.JSInterop;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddLocalization();
+builder.Services
+    .AddScoped(sp =>
+        new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddLocalization()
+    .AddBlazorise(options =>
+         {
+             options.Immediate = true;
+         })
+        .AddBootstrap5Providers()
+        .AddFontAwesomeIcons();
 
 var host = builder.Build();
 
