307cedc605d3fe70dbd28d96e71602ed0ee3bdc2
Gil Shalit <gil.shalit@gmail.com>
2024-04-10 18:20:39 +0300

    Revert "Localization (not working)"
    This reverts commit 371e9ab926f09c4da22d922d9debc451a14e510f.

--- clientHasidicStories/Program.cs
+++ clientHasidicStories/Program.cs
@@ -1,19 +1,11 @@
 using clientHasidicStories;
-using clientHasidicStories.Extensions;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Localization;
-
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app=builder.Build();
-
-await app.SetDefaultCulture();
-
-await app.RunAsync();
+await builder.Build().RunAsync();
