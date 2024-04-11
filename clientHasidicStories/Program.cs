c73c4716361d88229630d74de84a7fbae49a29e9
Gil Shalit <gil.shalit@gmail.com>
2024-04-11 17:46:28 +0300

    formatting list controls and endpoint configuration

--- clientHasidicStories/Program.cs
+++ clientHasidicStories/Program.cs
@@ -12,6 +12,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Build the configuration
+var configuration = builder.Configuration;
+
+// Add services to the service collection
 builder.Services
     .AddScoped(sp =>
         new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
@@ -20,22 +24,21 @@ builder.Services
          {
              options.Immediate = true;
          })
-        .AddBootstrap5Providers()
-        .AddFontAwesomeIcons();
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons()
+    .AddScoped(sp => //need to modify base address for the HttpClient with Configuration["endpointURL"]
+        new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 var host = builder.Build();
 
 const string defaultCulture = "he-IL";
-
 var js = host.Services.GetRequiredService<IJSRuntime>();
 var result = await js.InvokeAsync<string>("blazorCulture.get");
 var culture = CultureInfo.GetCultureInfo(result ?? defaultCulture);
-
 if (result == null)
 {
     await js.InvokeVoidAsync("blazorCulture.set", defaultCulture);
 }
-
 CultureInfo.DefaultThreadCurrentCulture = culture;
 CultureInfo.DefaultThreadCurrentUICulture = culture;
 
