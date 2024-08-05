7efb375e919f14d6e7d7dd735a202abd3f010895
Gil Shalit <gil.shalit@gmail.com>
2024-08-05 19:19:13 +0300

    with points and popup and dblclick

--- clientHasidicStories/wwwroot/scripts/map.js
+++ clientHasidicStories/wwwroot/scripts/map.js
@@ -2,46 +2,86 @@
 let circleRadius = 6;
 let originalOpacity = 0.5;
 
-window.storyMap = (center, divname, date) => {
+window.storyMap = (center, divname, data) => {
     maptilersdk.config.apiKey = 'yHCSCgx2fJ24IoOXLGYO';
     map = new maptilersdk.Map({
         container: divname, // container's id or the HTML element to render the map
         style: maptilersdk.MapStyle.TOPO.TOPOGRAPHIQUE,
         center: center, // starting position [lng, lat]
-        zoom: 3, // starting zoom
+        zoom: 4, // starting zoom
         pitch: 90,
         //bearing: -100.86,
         //maxPitch: 85,
         maxZoom: 14
     });
 
-//    map.addLayer({
-//        'id': 'points',
-//        'type': 'circle',
-//        'source': 'points',
-//        'paint': {
-//            'circle-radius': circleRadius,
-//            // Start with an opacity of 0
-//            'circle-color': '#B42222',
-//            'circle-opacity': 0
-//        },
-//    });
+    map.on('load', () => {
+        var popup = new maptilersdk.Popup({
+            closeButton: false,
+            closeOnClick: false
+        });
 
-//    // Animate the opacity
-//    let opacity = 0;
-//    const maxOpacity = originalOpacity; // Target opacity
-//    const animationStep = 0.05; // Incremental step
-//    const interval = 10; // Time in milliseconds between each step
+        map.addSource('points', data);
 
-//    const intervalId = setInterval(() => {
-//        opacity += animationStep;
-//        if (opacity >= maxOpacity) {
-//            opacity = maxOpacity;
-//            clearInterval(intervalId); // Stop the animation when the target opacity is reached
-//        }
-//        map.setPaintProperty('points', 'circle-opacity', opacity);
-//    }, interval);
+        map.addLayer({
+            'id': 'points',
+            'type': 'circle',
+            'source': 'points',
+            'paint': {
+                'circle-radius': circleRadius,
+                // Start with an opacity of 0
+                'circle-color': '#B42222',
+                'circle-opacity': 0
+            },
+        });
 
+        // Animate the opacity
+        let opacity = 0;
+        const maxOpacity = 1; // Target opacity
+        const animationStep = 0.05; // Incremental step
+        const interval = 10; // Time in milliseconds between each step
+
+        const intervalId = setInterval(() => {
+            opacity += animationStep;
+            if (opacity >= maxOpacity) {
+                opacity = maxOpacity;
+                clearInterval(intervalId); // Stop the animation when the target opacity is reached
+            }
+            map.setPaintProperty('points', 'circle-opacity', opacity);
+        }, interval);
+
+        map.on('mouseenter', 'points', function (evt) {
+            // Change the cursor to a pointer when it enters a feature in the 'points' layer.
+            map.getCanvas().style.cursor = 'pointer';
+
+            if (evt.features.length > 0) {
+                const feature = evt.features[0];
+                var coordinates = feature.geometry.coordinates.slice();
+                var name = feature.properties.name;
+                var link = feature.properties.link;
+
+                popup.setLngLat(coordinates) // sets the popup's location
+                    .setHTML(`<strong>${name}</strong><p style="padding=0;margin:0">double click to go there</p>`) // sets the popup's content
+                    .addTo(map); // adds the popup to the map
+            }
+        });
+        map.on('mouseleave', 'points', function () {
+            // Change it back to the default when it leaves.
+            map.getCanvas().style.cursor = '';
+            popup.remove();
+        });
+        map.on('dblclick', 'points', function (evt) {
+            if (evt.features.length > 0) {
+                const feature = evt.features[0];
+                var link = feature.properties.link; // Get the link from the feature's properties
+
+                // Open the link in a new tab
+                if (link) { // Check if the link is not undefined or empty
+                    window.open(link, '_blank');
+                }
+            }
+        });
+    });
 }
 
 window.initializeMap = (center, divname, dotNetRef) => {
