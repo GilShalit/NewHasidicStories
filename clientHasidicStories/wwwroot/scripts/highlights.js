dd18eda76ac138f08dbd9c51e2b35b4ccc397f5f
Gil Shalit <gil.shalit@gmail.com>
2024-08-15 10:41:29 +0300

    select places and persons on Story page.

---
+++ clientHasidicStories/wwwroot/scripts/highlights.js
@@ -0,0 +1,12 @@
+﻿window.highlight = (type, id, isHighlight) => {
+    let pbPage = document.querySelector('pb-page');
+    let pbView = pbPage.querySelector('pb-view');
+    let elements = pbView.shadowRoot.querySelectorAll(type);
+    let color = type == 'persname' ? '#F1E5AC' : '#FDD7E4';
+    elements.forEach(element => {
+        if (element.getAttribute('ref') === '#' + id) {
+            if (isHighlight) element.style.backgroundColor = color;
+            else element.style.backgroundColor = '';
+        }
+    });
+}
